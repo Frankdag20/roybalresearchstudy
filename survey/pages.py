4923d93bf692d9d1bb58707dbf28bb808f319705
Frank D'Agostino <frankdag20@gmail.com>
2020-04-04 00:26:42 -0400

    added timer things and beginning sequence

--- survey/pages.py
+++ survey/pages.py
@@ -3,9 +3,9 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 import datetime
+import time
 
 # currentDT = datetime.datetime.now()
-
 
 class Start(Page):
     form_model = 'player'
@@ -19,10 +19,31 @@ class Start(Page):
 
 class Wait(Page):
     form_model = 'player'
+
     #timer_text = "See you tomorrow!"
     #timeout_seconds = 10
     # time = datetime.now()
     # form_fields = [time]
+
+class Next(Page):
+    form_model = 'player'
+    timeout_seconds = 600
+
+class Idle(Page):
+    form_model = 'player'
+
+class Start(Page):
+    form_model='player'
+
+    timeout_seconds = 10
+
+    def is_displayed(self):
+        return self.round_number == 1
+
+    def before_next_page(self):
+        import time
+
+        self.participant.vars['expiry'] = time.time() + 1440 * 60
 
 class Intro(Page):
     form_model = 'player'
@@ -30,35 +51,59 @@ class Intro(Page):
 class MyPage(Page):
     form_model = 'player'
     form_fields = ['Page1affirm']#, 'affirm2', 'affirm3']
-#    def get_timeout_seconds(self):
-#        return self.participant.vars['expiry'] - time.time()
 
-#    def is_displayed(self):
-#        return self.get_timeout_seconds() > 3
+    def get_timeout_seconds(self):
+        return self.participant.vars['expiry'] - time.time()
+
+    def is_displayed(self):
+        return self.get_timeout_seconds() > 3
 
 class MyPage2(Page):
     form_model = 'player'
     form_fields = ['Page2healthM']#, 'healthP2', 'healthP3']
+
+    def get_timeout_seconds(self):
+        return self.participant.vars['expiry'] - time.time()
+
+    def is_displayed(self):
+        return self.get_timeout_seconds() > 3
 
 class MyPage3(Page):
     form_model = 'player'
     form_fields = ['Page3healthT']
     #timeout_seconds = 60
 
+    def get_timeout_seconds(self):
+        return self.participant.vars['expiry'] - time.time()
+
+    def is_displayed(self):
+        return self.get_timeout_seconds() > 3
+
 class MyPage4(Page):
     form_model = 'player'
     form_fields = ['Page5mood']
+
+    def get_timeout_seconds(self):
+        return self.participant.vars['expiry'] - time.time()
+
+    def is_displayed(self):
+        return self.get_timeout_seconds() > 3
 
 class MyPage5(Page):
     form_model = 'player'
     form_fields = ['info5', 'info6', 'info7', 'info8', 'info9']
 
+    def get_timeout_seconds(self):
+        return self.participant.vars['expiry'] - time.time()
+
+    def is_displayed(self):
+        return self.get_timeout_seconds() > 3
 
 class Results(Page):
     pass
 
 
-page_sequence = [Intro, MyPage, MyPage2, MyPage4, Wait,
-                 Intro, MyPage, MyPage2, Wait,
-                 Intro, MyPage, MyPage2, Wait,
+page_sequence = [Idle, Start, Intro, MyPage, MyPage2, MyPage4, Wait, Next,
+                 Intro, MyPage, MyPage2, Wait, Next,
+                 Intro, MyPage, MyPage2, Wait, Next,
                  Intro, MyPage, MyPage2]
