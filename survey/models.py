e48b39382e5a8ee133b1d3afc28617cc60473de6
Frank D'Agostino <frankdag20@gmail.com>
2020-05-04 04:12:03 -0400

    made small replacement in get_value method

--- survey/models.py
+++ survey/models.py
@@ -82,7 +82,7 @@ class Player(BasePlayer):
     affirm_answer = models.StringField(widget=widgets.RadioSelect)
 
     def get_value(self):
-        return self.session.vars['affirm_file'][self.sessions.vars['val_file'][self.player.id_in_group]['value']]
+        return self.player.vars['affirm_file'][self.sessions.vars['val_file'][self.player.id_in_group]['value']]
 
     def current_question(self):
         return self.session.vars['questions'][self.daysurv]
