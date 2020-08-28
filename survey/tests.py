485277b52bd3580226588bb913702725bce9b75f
Frank D'Agostino <frankdag20@gmail.com>
2020-08-28 00:52:52 -0400

    updates on files

--- survey/tests.py
+++ survey/tests.py
@@ -1,4 +1,5 @@
-from otree.api import Currency as c, currency_range
+from otree.api import Currency as c, currency_range, expect
+
 from . import pages
 from ._builtin import Bot
 from .models import Constants
@@ -6,4 +7,13 @@ from .models import Constants
 
 class PlayerBot(Bot):
     def play_round(self):
-        pass
+
+        yield pages.Demographics, dict(age=24, gender='Male')
+
+        yield (
+            pages.CognitiveReflectionTest,
+            dict(crt_bat=10, crt_widget=5, crt_lake=48),
+        )
+
+        for value in [self.player.crt_bat, self.player.payoff]:
+            expect(value, '!=', None)
