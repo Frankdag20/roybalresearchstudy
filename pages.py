9d1d85da89dd8d171cb42fe86114955b03bce040
Frank D'Agostino <frankdag20@gmail.com>
2020-08-28 01:16:54 -0400

    another test to get timer to work

---
+++ pages.py
@@ -0,0 +1,27 @@
+from otree.api import Currency as c, currency_range
+
+from ._builtin import Page, WaitPage
+from .models import Constants
+import time
+
+class Demographics(Page):
+    def is_displayed(self):
+        return self.round_number == 1
+
+    def before_next_page(self):
+        # user has 5 minutes to complete as many pages as possible
+        self.participant.vars['expiry'] = time.time() + 1 * 60
+    form_model = 'player'
+    form_fields = ['age', 'gender']
+
+
+class CognitiveReflectionTest(Page):
+    form_model = 'player'
+    form_fields = ['crt_bat', 'crt_widget', 'crt_lake']
+    def get_timeout_seconds(self):
+        return self.participant.vars['expiry'] - time.time()
+
+    def is_displayed(self):
+        return self.get_timeout_seconds() > 3
+
+page_sequence = [Demographics, CognitiveReflectionTest]
--- survey/test.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import calendar
-import smtplib
-import os
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-import sendEmail
-
-sendEmail.send_email(1)
