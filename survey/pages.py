5ef5c9a7c82e88b260f262e5dc5650c778bbcfde
Frank D'Agostino <frankdag20@gmail.com>
2020-08-28 04:25:15 -0400

    put email function right into pages.py

--- survey/pages.py
+++ survey/pages.py
@@ -8,10 +8,58 @@ import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-import sendEmail.py
+# import sendEmail.py
 
-import importlib
-importlib.import_module('sendEmail')
+# import importlib
+# importlib.import_module('sendEmail')
+
+def send_email(participant):
+    now = datetime.datetime.now()
+
+    today_date = datetime.date.today()  # today's date
+
+    cy = now.year  # current year
+    cm = now.month  # current month
+    cd = now.day  # current day
+    participant = participant + 1
+    FROM = "fdagostinoj@gmail.com"
+    # TO = ["frankdag20@gmail.com"]  # must be a list
+    TO = ["frankdag20@gmail.com"]  # must be a list
+
+    SUBJECT = "Hello!"
+    TEXT = """Hello,
+
+     This is an automatic email notifying you that Participant 1 has not yet filled out the survey for today."""
+
+    # Prepare actual message
+    # message = """From: %s To: %s Subject: %s
+    #
+    # %s
+    # """ % (FROM, ", ".join(TO), "Hello", TEXT)
+
+    # Prepare actual message
+    message = """Subject: %s
+
+    %s
+     """ % ("Research Notification", TEXT)
+
+    # Send the mail
+    username = str("fdagostinoj@gmail.com")
+    password = str("dagostino1")
+
+    server = smtplib.SMTP("smtp.gmail.com", 587, timeout=30)
+    server.set_debuglevel(1)
+
+    try:
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(FROM, TO, message)
+        print("The reminder e-mail for WK-2 was sent !")
+    except:
+        print("Couldn't send e-mail regarding WK-2")
+    finally:
+        server.quit()
+    # input("Press any key to exit..")
 
 from datetime import datetime
 import time
@@ -51,7 +99,7 @@ class Start(Page):
     def is_displayed(self):
 
         if self.get_timeout_seconds() == 0:
-            self.player.notif(self.player.id_in_group)
+            send_email(self.player.id_in_group)
 
         return self.get_timeout_seconds() != 0
 
