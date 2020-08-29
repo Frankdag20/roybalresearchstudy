e515af38bc68b0d10525ff6c4985fb646dcda3ed
Frank D'Agostino <frankdag20@gmail.com>
2020-08-28 23:44:32 -0400

    trying out sendGrid

--- survey/pages.py
+++ survey/pages.py
@@ -19,6 +19,28 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+def sendGrid_email():
+    message = Mail(
+        from_email='fdagostinoj@gmail.com',
+        to_emails='frankdag20@gmail.com',
+        subject='Sending with Twilio SendGrid is Fun',
+        html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
+
+
 def send_email(participant):
     #now = datetime.datetime.now()
 
@@ -103,6 +125,7 @@ class Start(Page):
         return (self.participant.vars['expiry'] - int(y))*3600
 
     def before_next_page(self):
+        sendGrid_email()
         self.player.email_send()
         send_email(self.player.id_in_group)
 
