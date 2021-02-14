5fa1f335b9916c7682acc0c35bdd81c4e2ad21e5
Frank D'Agostino <frankdag20@gmail.com>
2021-02-14 06:48:30 -0500

    simplified things again in slider

--- survey/pages.py
+++ survey/pages.py
@@ -304,7 +304,7 @@ class MyPage2_all(Page):
 
     def get_form_fields(self):
 
-        return conf_array[self.player.day_track-1], 'checkslider',
+        return conf_array[self.player.day_track-1],
 
     def checkslider_error_message(self, value):
             if not value:
